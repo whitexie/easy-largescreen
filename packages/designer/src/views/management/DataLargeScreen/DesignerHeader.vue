df3c21fa76b6a6272050d81db0c6ca43a1fa0552
Ysansan <ysansan98@outlook.com>
2024-08-21 17:15:15 +0800

    feat(大屏设计器): :sparkles: 页面增加返回功能

--- packages/designer/src/views/management/DataLargeScreen/DesignerHeader.vue
+++ packages/designer/src/views/management/DataLargeScreen/DesignerHeader.vue
@@ -1,20 +1,28 @@
 <script lang="ts" setup>
+import { useRouter } from 'vue-router';
+import { Icon } from '@iconify/vue/dist/iconify.js';
 import Menus from '@/components/Designer/Menus/Menus.vue';
 import { useLargeScreenDesigner } from '@/stores/designer';
 
 const designerStore = useLargeScreenDesigner();
+const router = useRouter();
+
+function handleBack() {
+  router.back();
+}
 </script>
 
 <template>
   <div class="px-2">
     <div class="border-b-1px gap-2 flex items-center justify-between border-gray-200 border-b-solid h-50px">
-      <div class="left">
+      <div class="start flex items-center gap-1 cursor-pointer" @click="handleBack">
+        <Icon class="" icon="ion:arrow-back" />
         <n-input v-model:value="designerStore.state.name" placeholder="大屏名称" class="w-160px!" />
       </div>
       <div class="center flex-1 h-full">
         <Menus />
       </div>
-      <div class="right">
+      <div class="end">
         <div class="flex items-center gap-2">
           <n-button>预览</n-button>
           <n-button type="primary">
