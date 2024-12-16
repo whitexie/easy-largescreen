945324a6cefe62948fa61738d7f5e20202e2cf31
ysansan <hippeauxie@gmail.com>
2024-12-16 15:29:52 +0800

    refactor(SelectedWidgetsBounding): fix css style

--- apps/designer/src/views/management/DataLargeScreen/components/SelectedWidgetsBounding.vue
+++ apps/designer/src/views/management/DataLargeScreen/components/SelectedWidgetsBounding.vue
@@ -14,23 +14,16 @@ const { selectionBounds } = storeToRefs(designerStore);
 const style = computed(() => {
   const { x: left = 0, y: top = 0, width = 0, height = 0 } = selectionBounds.value || {};
   return {
-    left: `${left}px`,
-    top: `${top}px`,
-    width: `${width}px`,
-    height: `${height}px`,
+    'left': `${left}px`,
+    'top': `${top}px`,
+    'width': `${width}px`,
+    'height': `${height}px`,
+    'box-shadow': ` 0 0 6px ${boxShadowColor.value}`,
+    'border': `1px solid ${primaryColor.value}`,
   };
 });
 </script>
 
 <template>
-  <div v-show="selectionBounds" class="absolute selected-widgets-bounding" :style="style" />
+  <div v-show="selectionBounds" class="absolute cursor-move pointer-events-none" :style="style" />
 </template>
-
-<style lang="less" scoped>
-.selected-widgets-bounding {
-  box-shadow: 0 0 6px v-bind(boxShadowColor);
-  border: 1px solid v-bind(primaryColor);
-  cursor: move;
-  pointer-events: none;
-}
-</style>
